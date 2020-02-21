d7ff90d9fb579653cc2ea8a417f6e75e3bbfc69f
Dinatolemy <dina.a.tolemy@gmail.com>
2020-02-21 11:56:56 +1100

    heroku db connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Rody@1234",
-  database: "Burger_db"
-});
+var connection; 
+
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection= mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Rody@1234",
+    database: "Burger_db"
+  });
+}
+
 
 connection.connect(function(err) {
     if (err) {
